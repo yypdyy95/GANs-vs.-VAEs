be846a4553024df4f2f40e7ce69be6f88c9b98ed
FlashTek <rzrolandzimmermann@live.de>
2017-07-10 19:02:39 +0200

    .

--- image_parser.py
+++ image_parser.py
@@ -2,6 +2,7 @@ import PIL
 from PIL import Image
 import os
 import numpy as np
+import gzip
 '''
 convert to channels_last format
 '''
@@ -13,6 +14,10 @@ convert to channels_last format
 def convert_image_cl(img):
     return np.swapaxes(np.swapaxes(np.array(img), 0, 1), 1, 2)
 
+def resize_image(img, width, height):
+    hsize = height
+    return img.resize((width,hsize), PIL.Image.ANTIALIAS)
+
 '''
 load_data_set:
 arguments:
@@ -23,10 +28,6 @@ arguments:
 
 def load_data_set(name, maxitems=12500, image_dim = 64):
     import dill
-    def resize_image(img, width, height):
-
-        hsize = height
-        return img.resize((width,hsize), PIL.Image.ANTIALIAS)
 
     def internal_load_data(maxitems=10000):
         import glob
@@ -54,27 +55,23 @@ def load_data_set(name, maxitems=12500, image_dim = 64):
 
     return data
 
-def load_data_set_classes(name, class_name, maxitems=12500, image_dim = 64, cf=False):
+def load_data_set_classes(name, class_names, maxitems=12500, image_dim = 64, cf=False, header=0, delimiter=' '):
     import dill
     import pandas
-
-    def resize_image(img, width, height):
-        hsize = height
-        return img.resize((width,hsize), PIL.Image.ANTIALIAS)
 
     def internal_load_data(maxitems=10000):
         import glob
         from scipy import misc
 
-        csv = pandas.read_csv("./../datasets/celeba/annotations.csv", header=0, delimiter=' ')
+        csv = pandas.read_csv("./../datasets/" + name + "/annotations.csv", header=header, delimiter=delimiter)
 
-        filenames = glob.glob("./../datasets/"+ name + "/img_align_celeba/*.jpg")
+        filenames = glob.glob("./../datasets/"+ name + "/images/*.jpg")
 
         if cf:
             images = np.empty((maxitems, 3, image_dim, image_dim), dtype=np.uint8)
         else:
             images = np.empty((maxitems, image_dim, image_dim, 3), dtype=np.uint8)
-        classes = np.empty((maxitems, 1))
+        classes = np.empty((maxitems, len(class_names)))
 
         for i in range(maxitems):
             img = Image.open(filenames[i])
@@ -83,20 +80,79 @@ def load_data_set_classes(name, class_name, maxitems=12500, image_dim = 64, cf=F
             if cf:
                 img = convert_image_cf(img)
             images[i] = img
-            classes[i, 0] = csv[class_name][i]
+            for n, class_name in enumerate(class_names):
+                classes[i, n] = csv.loc[csv['name'] == os.path.basename(filenames[i])][class_name]
 
         return images, classes
 
     if (os.path.isfile("./../datasets/" + name + ".pkl")):
-        with open("./../datasets/" + name + '.pkl', 'rb') as f:
+        with gzip.GzipFile("./../datasets/" + name + '.pkl', 'rb') as f:
             data = dill.load(f)
-        with open("./../datasets/" + name + '.classes.pkl', 'rb') as f:
+        with gzip.GzipFile("./../datasets/" + name + '.classes.pkl', 'rb') as f:
             classes = dill.load(f)
     else:
         data, classes = internal_load_data(maxitems)
-        with open("./../datasets/" + name + '.pkl', 'wb') as f:
+        with gzip.GzipFile("./../datasets/" + name + '.pkl', 'wb') as f:
             dill.dump(data, f)
-        with open("./../datasets/" + name + '.classes.pkl', 'wb') as f:
+        with gzip.GzipFile("./../datasets/" + name + '.classes.pkl', 'wb') as f:
             dill.dump(classes, f)
 
     return data, classes
+
+
+def load_char74k(image_dim = 64, cf=False):
+    import dill
+
+    def internal_load_data():
+        path = "./../datasets/char74k/images"
+
+        #count all images
+        import os
+        nb_files = sum([len(files) for r, d, files in os.walk(path)])
+
+        if cf:
+            images = np.empty((nb_files, 3, image_dim, image_dim), dtype=np.uint8)
+        else:
+            images = np.empty((nb_files, image_dim, image_dim, 3), dtype=np.uint8)
+        y = np.empty((nb_files, 2))
+
+        def read_images(image_type, name):
+            nonlocal index
+            for r, d, files in os.walk(path + "/" + name):
+                for f in files:
+                    img = Image.open(r + "/" + f)
+
+                    img = resize_image(img, image_dim, image_dim)
+                    if cf:
+                        img = convert_image_cf(img)
+
+                    if img.mode != "L":
+                        images[index] = img
+                    else:
+                        images[index] = img.convert(mode="RGB")
+
+                    y[index, 0] = int(r[-3:])
+                    y[index, 1] = image_type
+
+                    index += 1
+
+        index = 0
+        read_images(0, "Fnt")
+        read_images(1, "Hnd")
+        read_images(2, "Img")
+
+        return images, y
+
+    if (os.path.isfile("./../datasets/char74k.pkl")):
+        with gzip.GzipFile("./../datasets/char74k.pkl", 'rb') as f:
+            images = dill.load(f)
+        with gzip.GzipFile("./../datasets/char74k.classes.pkl", 'rb') as f:
+            y = dill.load(f)
+    else:
+        images, y = internal_load_data()
+        with gzip.GzipFile("./../datasets/char74k.pkl", 'wb') as f:
+            dill.dump(images, f)
+        with gzip.GzipFile("./../datasets/char74k.classes.pkl", 'wb') as f:
+            dill.dump(y, f)
+
+    return images, y
