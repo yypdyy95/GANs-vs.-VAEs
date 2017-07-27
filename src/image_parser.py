393a07e2be9f30e7776955708b23f11c3c02a0cc
FlashTek <rzrolandzimmermannqlive.de>
2017-07-27 11:41:58 +0200

    .

--- src/image_parser.py
+++ src/image_parser.py
@@ -3,37 +3,42 @@ from PIL import Image
 import os
 import numpy as np
 import gzip
-'''
-convert to channels_last format
-'''
+
+"""
+    convert to channels_last format
+"""
 def convert_image_cf(img):
     return np.swapaxes(np.swapaxes(np.array(img), 2, 1), 1, 0)
-'''
-convert to channels_last format
-'''
+
+"""
+    convert to channels_last format
+"""
 def convert_image_cl(img):
     return np.swapaxes(np.swapaxes(np.array(img), 0, 1), 1, 2)
 
+"""
+    resize the image
+"""
 def resize_image(img, width, height):
     hsize = height
     return img.resize((width,hsize), PIL.Image.ANTIALIAS)
 
-'''
-load_data_set:
+"""
+    load the data set from path with the specified name
 arguments:
     - name:      one of 'cats', 'dogs' or 'celeb'
     - maxitems:  number of images to be loaded
     - image_dim: image dimesion = (3, image_dim, image_dim)
-'''
-
-def load_data_set(name, maxitems=12500, image_dim = 64):
+    - path:      relative or absolute path to the datasets directory in which the raw and final data is saved/will be saved
+"""
+def load_data_set(name, maxitems=12500, image_dim = 64, path = "./../../datasets/"):
     import dill
 
     def internal_load_data(maxitems=10000):
         import glob
         from scipy import misc
 
-        filenames = glob.glob("./../datasets/"+ name +"/*.jpg")
+        filenames = glob.glob(os.path.join(path, name +"/*.jpg"))
         images = np.empty((maxitems, 3, image_dim, image_dim), dtype=np.uint8)
 
         for i in range(maxitems):
@@ -45,17 +50,26 @@ def load_data_set(name, maxitems=12500, image_dim = 64):
 
         return images
 
-    if (os.path.isfile("./../datasets/" + name + ".pkl")):
-        with open("./../datasets/" + name + '.pkl', 'rb') as f:
+    if (os.path.isfile(os.path.join(path, name + ".pkl"))):
+        with open(os.path.isfile(os.path.join(path, name + ".pkl")), 'rb') as f:
             data = dill.load(f)
     else:
         data = internal_load_data(maxitems)
-        with open("./../datasets/" + name + '.pkl', 'wb') as f:
+        with open(os.path.join(path, name + '.pkl'), 'wb') as f:
             dill.dump(data, f)
 
     return data
 
-def load_data_set_classes(name, class_names, maxitems=12500, image_dim = 64, cf=False, header=0, delimiter=' '):
+"""
+    load the data set and the class labels specified in a csv file
+    arguments:
+        - name:         one of 'cats', 'dogs' or 'celeb'
+        - maxitems:     number of images to be loaded
+        - image_dim:    image dimesion = (3, image_dim, image_dim)
+        - path:         relative or absolute path to the datasets directory in which the raw and final data is saved/will be saved
+        - class_names:  listof class names which will be loaded from the annotations.csv file
+"""
+def load_data_set_classes(name, class_names, maxitems=12500, image_dim = 64, cf=False, path="./../datasets/", header=0, delimiter=' '):
     import dill
     import pandas
 
@@ -63,9 +77,9 @@ def load_data_set_classes(name, class_names, maxitems=12500, image_dim = 64, cf=
         import glob
         from scipy import misc
 
-        csv = pandas.read_csv("./../datasets/" + name + "/annotations.csv", header=header, delimiter=delimiter)
+        csv = pandas.read_csv(os.path.join(path, name + "/annotations.csv"), header=header, delimiter=delimiter)
 
-        filenames = glob.glob("./../datasets/"+ name + "/images/*.jpg")
+        filenames = glob.glob(os.path.join(path, name + "/images/*.jpg"))
 
         if cf:
             images = np.empty((maxitems, 3, image_dim, image_dim), dtype=np.uint8)
@@ -88,30 +102,37 @@ def load_data_set_classes(name, class_names, maxitems=12500, image_dim = 64, cf=
 
         return images, classes
 
-    if (os.path.isfile("./../datasets/" + name + ".pkl")):
-        with gzip.GzipFile("./../datasets/" + name + '.pkl', 'rb') as f:
+    if (os.path.isfile(os.path.join(path, name + ".pkl"))):
+        with gzip.GzipFile(os.path.join(path, name + '.pkl)', 'rb') as f:
             data = dill.load(f)
-        with gzip.GzipFile("./../datasets/" + name + '.classes.pkl', 'rb') as f:
+        with gzip.GzipFile(os.path.join(path, name + '.classes.pkl'), 'rb') as f:
             classes = dill.load(f)
     else:
         data, classes = internal_load_data(maxitems)
-        with gzip.GzipFile("./../datasets/" + name + '.pkl', 'wb') as f:
+        with gzip.GzipFile(os.path.join(path, name + '.pkl'), 'wb') as f:
             dill.dump(data, f)
-        with gzip.GzipFile("./../datasets/" + name + '.classes.pkl', 'wb') as f:
+        with gzip.GzipFile(os.path.join(path, name + '.classes.pkl'), 'wb') as f:
             dill.dump(classes, f)
 
     return data, classes
 
-
-def load_char74k(image_dim = 64, cf=False):
+"""
+    load the char74k data set
+    arguments:
+        - image_dim: image dimesion = (3, image_dim, image_dim)
+        - path:      relative or absolute path to the datasets directory in which the raw and final data is saved/will be saved
+"""
+def load_char74k(image_dim = 64, cf=False, path="./../../datasets/"):
     import dill
 
+    path += "/char74k/"
+
     def internal_load_data():
-        path = "./../datasets/char74k/images"
+        image_path = path + "images"
 
         #count all images
         import os
-        nb_files = sum([len(files) for r, d, files in os.walk(path)])
+        nb_files = sum([len(files) for r, d, files in os.walk(image_path)])
 
         if cf:
             images = np.empty((nb_files, 3, image_dim, image_dim), dtype=np.uint8)
@@ -121,7 +142,7 @@ def load_char74k(image_dim = 64, cf=False):
 
         def read_images(image_type, name):
             nonlocal index
-            for r, d, files in os.walk(path + "/" + name):
+            for r, d, files in os.walk(image_path + "/" + name):
                 for f in files:
                     img = Image.open(r + "/" + f)
 
@@ -146,16 +167,16 @@ def load_char74k(image_dim = 64, cf=False):
 
         return images, y
 
-    if (os.path.isfile("./../datasets/char74k.pkl")):
-        with gzip.GzipFile("./../datasets/char74k.pkl", 'rb') as f:
+    if (os.path.isfile(path + "char74k.pkl")):
+        with gzip.GzipFile(path + "char74k.pkl", 'rb') as f:
             images = dill.load(f)
-        with gzip.GzipFile("./../datasets/char74k.classes.pkl", 'rb') as f:
+        with gzip.GzipFile(path + "char74k.classes.pkl", 'rb') as f:
             y = dill.load(f)
     else:
         images, y = internal_load_data()
-        with gzip.GzipFile("./../datasets/char74k.pkl", 'wb') as f:
+        with gzip.GzipFile(path + "char74k.pkl", 'wb') as f:
             dill.dump(images, f)
-        with gzip.GzipFile("./../datasets/char74k.classes.pkl", 'wb') as f:
+        with gzip.GzipFile(path + "char74k.classes.pkl", 'wb') as f:
             dill.dump(y, f)
 
     return images, y
